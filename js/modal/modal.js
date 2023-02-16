5d9997c9f191ea3d01a3d4b761958a1f73a4b309
natasha-gri <natashagri.dw@gmail.com>
2023-02-16 10:27:49 +0500

    17.02.2023-12-ИТОГ
    доделать:
    1)слайдер отзывов
    2)отдельный экран (первый) мобилки

--- js/modal/modal.js
+++ js/modal/modal.js
@@ -24,22 +24,6 @@ function someFunc() {
   });
   }
 
-// // модалка для кнопки меню
-// const modalAdd = document.querySelector('.modal__add'), //модалка
-//   addAd = document.querySelector('.add__ad');	//кнопка
-  
-
-// addAd.addEventListener('click', () => {
-//   modalAdd.classList.remove('hide');
-// });
-
-// modalAdd.addEventListener('click', (event) => {
-//   const target = event.target;
-
-//   if(target.classList.contains('modal__close') ||  target === modalAdd)
-//     modalAdd.classList.add('hide');
-// });
-
 // модалка для видео
 const modalVideoAdd = document.querySelector('.modal-video__add'), //модалка
   addVideoAd = document.querySelector('.video__ad');	//кнопка
